71fcaefd0a6b9bf8b4dd226a01a082f3d167d878
egor <ega.nik5674@gmail.com>
2024-05-16 18:48:23 +0300

    Update users app

---
+++ users/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+
+from users.models import User
+from users.validators import CheckPassword
+
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    """Сериализатор для регистрации пользователя"""
+
+    password = serializers.CharField(write_only=True)
+    passwordConfirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ("first_name", "last_name", "email", "tg_username", "password", "passwordConfirm")
+        validators = [CheckPassword()]
+
+    def create(self, validated_data):
+        validated_data.pop("passwordConfirm")
+        password = validated_data.pop("password")
+        user = User.objects.create(**validated_data)
+        user.set_password(password)
+        user.save()
+
+        return user
