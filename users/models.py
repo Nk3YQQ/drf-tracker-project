efe643d380e7e8b954d11cae390590330aa29b90
egor <ega.nik5674@gmail.com>
2024-05-18 19:28:06 +0300

    Update users app

--- users/models.py
+++ users/models.py
@@ -1,3 +1,6 @@
+import uuid
+
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -9,7 +12,9 @@ class User(AbstractUser):
     last_name = models.CharField(max_length=150, verbose_name="Фамилия")
 
     email = models.EmailField(unique=True, verbose_name="Электронная почта")
-    tg_username = models.CharField(unique=True, max_length=150, verbose_name="Имя пользователя в Телеграмме")
+
+    tg_token = models.UUIDField(default=uuid.uuid4, unique=True, editable=False, verbose_name="Токен")
+    chat_id = models.CharField(max_length=255, **settings.NULLABLE, verbose_name="Идентификатор чата")
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
