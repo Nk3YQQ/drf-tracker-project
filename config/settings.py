42150343aa3956b1d38288b60d5371cb38456c22
egor <ega.nik5674@gmail.com>
2024-06-05 10:45:56 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -93,9 +93,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
-        "HOST": os.getenv("POSTGRES_HOST"),
-        "PORT": int(os.getenv("POSTGRES_PORT"))
+        "HOST": os.getenv("POSTGRES_HOST")
     },
 }
 
