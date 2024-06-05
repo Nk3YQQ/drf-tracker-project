9e535f98fb761f7c2763574534a5df125bc5dc03
egor <ega.nik5674@gmail.com>
2024-06-05 12:35:56 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -93,7 +93,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": os.getenv("POSTGRES_DB"),
         "USER": os.getenv("POSTGRES_USER"),
-        "PASSWORD": os.getenv("POSTGRES_PASSWORD", "mysecretpassword")
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD")
     },
 }
 
